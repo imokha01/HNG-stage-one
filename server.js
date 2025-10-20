5a1da056c40d1c4db44b127271f101a2b546edf8
Ahmed Wahab <ahmed.wahab.imokha@gmail.com>
2025-10-20 14:43:49 +0100

    Created the express route

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import colors from 'colors';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import userRouter from './routers/router.js';
+
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+// Application Middleware Setup
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`.bgGreen.black);
